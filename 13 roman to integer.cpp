0df0dcdea952efa66e619208b14a94123ebea134
Aman Prajapati <pamanbpl15@gmail.com>
2022-07-06 03:14:58 +0530

    13 roman to integer.cpp

---
+++ 13 roman to integer.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    int romanToInt(string s) {
+        unordered_map<char, int> mpp;
+        mpp['I'] = 1;
+        mpp['V'] = 5;
+        mpp['X'] = 10;
+        mpp['L'] = 50;
+        mpp['C'] = 100;
+        mpp['D'] = 500;
+        mpp['M'] = 1000;
+
+	int result = mpp[s[s.length()-1]];
+		//cout << "Reesult = " << result<< endl;
+	for(int i = s.length()-2; i >= 0; i--)
+	{
+		if(mpp[s[i]] < mpp[s[i+1]])
+		result-=mpp[s[i]];
+		else
+		result+=mpp[s[i]];
+		//cout << "Result = " << result<< endl;
+	}
+//        int result = (mpp.end()-1).second;
+  return result;
+
+    }
+int main()
+{
+	cout << romanToInt("MCMXCIV");
+	return 0;
+}
+
